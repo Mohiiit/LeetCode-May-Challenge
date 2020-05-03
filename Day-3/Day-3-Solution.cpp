94fef51c2846247f3b9de773404e232e98474c7c
Mohiiit <48082542+Mohiiit@users.noreply.github.com>
2020-05-03 17:44:55 +0530

    Create Day-3-Solution.cpp

---
+++ Day-3/Day-3-Solution.cpp
@@ -0,0 +1,28 @@
+lass Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int a[26];
+        int b[26];
+        for(int i=0; i<26; i++)
+        {
+            a[i] = 0;
+            b[i] = 0;
+        }
+        for(int i=0; i<ransomNote.length(); i++)
+        {
+            a[ransomNote[i]-'a']++;
+        }
+        for(int i=0; i<magazine.length(); i++)
+        {
+            b[magazine[i]-'a']++;
+        }
+        for(int i=0; i<26; i++)
+        {
+            if(a[i]>b[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};

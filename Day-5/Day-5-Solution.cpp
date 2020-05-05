00251e8f7fbc78a18bc017c2dddff2afcdc1f65a
Mohiiit <48082542+Mohiiit@users.noreply.github.com>
2020-05-05 20:30:42 +0530

    Create Day-5-Solution.cpp

---
+++ Day-5/Day-5-Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+        int a[26] = {0};
+        for(int i =0; i<s.length(); i++)
+        {
+            a[s[i]-'a']++;
+        }
+        for(int i=0; i<s.length(); i++)
+        {
+            if(a[s[i]-'a']==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
